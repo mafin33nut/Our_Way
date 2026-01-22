e0a2c119b98d57d7bd0bb7a375ed27e28de51605
Mafin33nut <timofeiigonkin@yandex.ru>
2026-01-22 21:08:47 +0300

    docker mod2

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -54,7 +54,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     "rest_framework",
