609dc4a0be2265624483d2080ab6e3d8b04f05ed
Mafin33nut <timofeiigonkin@yandex.ru>
2026-01-22 22:24:06 +0300

    models fix

--- backend/app/models.py
+++ backend/app/models.py
@@ -6,14 +6,3 @@ class SampleModel(models.Model):
     def __str__(self):
         return self.name
 
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    bio = models.TextField(blank=True)
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
-    email_verified = models.BooleanField(default=False)
-    notify_by_email = models.BooleanField(default=True)  # предпочтение для получения email
-
-    def __str__(self):
-        return self.username
